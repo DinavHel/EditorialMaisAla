96364a3761b0f8dd5e01a25de1860a0e808a8917
a21adriangg <gonzalezgomez.adriangg@gmail.com>
2023-01-13 14:03:19 +0100

    Initial commit

---
+++ EditorialMaisAla/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+# Primeira vista
+def home(request):
+
+    res = open("EditorialMaisAla/Templates/home.html")
+    template = Template(res.read())
+    res.close()
+
+    cont = Context()
+    response = template.render(cont)
+
+    return HttpResponse(response)
+
+def books(request):
+    res = open("EditorialMaisAla/Templates/books.html")
+    template = Template(res.read())
+    res.close()
+
+    cont = Context()
+    response = template.render(cont)
+
+    return HttpResponse(response)
+
+def about(request):
+    res = open("EditorialMaisAla/Templates/about.html")
+    template = Template(res.read())
+    res.close()
+
+    cont = Context()
+    response = template.render(cont)
+
+    return HttpResponse(response)
+
+def contact(request):
+    res = open("EditorialMaisAla/Templates/contact.html")
+    template = Template(res.read())
+    res.close()
+
+    cont = Context()
+    response = template.render(cont)
+
+    return HttpResponse(response)
