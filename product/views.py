c31ba8307797f3488a8ea65ae31e4106d175b5f5
a21adriangg <gonzalezgomez.adriangg@gmail.com>
2023-02-06 09:05:25 +0100

    feat: Added urls to Category and Product

--- product/views.py
+++ product/views.py
@@ -1,18 +1,24 @@
 from django.http import Http404
 
-from rest_framework import APIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
 
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
-            return Product.objects.filter(category__slug = category_slug).get(slug = product_slug)
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
 
-    def get(self, request, category_slug, product_slug, format = None):
+    def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
 
 class CategoryDetail(APIView):
     def get_object(self, category_slug):
@@ -21,7 +27,7 @@ class CategoryDetail(APIView):
         except Category.DoesNotExist:
             raise Http404
 
-    def get(self, request, category_slug, format = None):
+    def get(self, request, category_slug, format=None):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
