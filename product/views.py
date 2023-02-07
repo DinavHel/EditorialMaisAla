9a9e86fec1845c74359c2d5c0d4ba9ec59a77dfb
a21adriangg <gonzalezgomez.adriangg@gmail.com>
2023-02-07 10:52:02 +0100

    fix: Fixed errors

--- product/views.py
+++ product/views.py
@@ -5,6 +5,13 @@ from rest_framework.response import Response
 
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.decorators import api_view
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
 
 class ProductDetail(APIView):
@@ -31,3 +38,15 @@ class CategoryDetail(APIView):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
